18e85966b317c8eeb055e77e8bbaea2092909655
dahald1 <123000339+dahald1@users.noreply.github.com>
2023-10-24 10:49:04 -0400

    Update even_odd_site.py
    found the bugs

--- programming files/even_odd_site.py
+++ programming files/even_odd_site.py
@@ -1,4 +1,5 @@
 # Bugs introduced: SH
+
 with open("numbers.html", "w") as f:
     # Write header to file
     f.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n")
@@ -6,12 +7,14 @@ with open("numbers.html", "w") as f:
     # Iterate through 50 integers
     for i in range(1, 50):
         # Write even numbers
+        #this line has a bug
         if i % 1 == 0: # Should be 2
             f.write("<tr><td>{}</td><td></td></tr>\n".format(i))
         # Write odd numbers
         else:
             f.write("<tr><td></td><td>{}</td></tr>\n".format(i))
     f.write("</table>\n</body>\n</html>")
+#this line also has a bug    
 exit(-1) # Early exit
 # Read from numbers
 with open("numbers.html", "r") as f:
