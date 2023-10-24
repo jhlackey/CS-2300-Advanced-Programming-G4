f42289e766052fb0d907022fa790991fc9360c59
dahald1 <123000339+dahald1@users.noreply.github.com>
2023-10-24 10:37:27 -0400

    Update rgb_to_hex.cc_bugs

--- programming files/rgb_to_hex.cc
+++ programming files/rgb_to_hex.cc
@@ -5,7 +5,7 @@
 std::string rgb_to_hex(int r, int g, int b)
 {
     r = std::max(0, std::min(255, r));
-    g = std::max(0, std::min(255, g));
+    g = std::max(0, min(255, g));  // this is a bug: should be std::
     b = std::max(0, std::min(255, b));
 
     // This is a string
@@ -13,7 +13,8 @@ std::string rgb_to_hex(int r, int g, int b)
     ss << std::uppercase << std::hex << std::setfill('0')
        << std::setw(2) << r << std::setw(2) << g << std::setw(2) << b;
 
-    return ss.str();
+    return str(); // bug: should be returning the ss.str()
 }
 
-//Test with std::string hexColor = rgb_to_hex(255, 127, 0); // returns "FF7F00"
+//Test with 
+std::string hexColor = rgb_to_hex(255, 127, 0); // returns "FF7F00"
