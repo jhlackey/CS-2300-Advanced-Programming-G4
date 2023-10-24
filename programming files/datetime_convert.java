ad4a190563f9b66827f06fc732be795662cc29cf
dahald1 <123000339+dahald1@users.noreply.github.com>
2023-10-24 10:57:59 -0400

    fixed the bugs

--- programming files/datetime_convert.java
+++ programming files/datetime_convert.java
@@ -5,6 +5,7 @@ public class datetime_convert {
     public static void main(String[] args) {
         // John bug
         String dateStr = "2022-03-17 10:45:30";
+        
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-DD HH:mm:ss");
         // Convert from string data type to LocalDateTIme
         LocalDateTime dateObj = LocalDateTime.parse(dateStr, formatter);
