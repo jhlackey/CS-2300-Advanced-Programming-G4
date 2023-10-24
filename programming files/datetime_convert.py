5f2246c7237954f24fb3856295a562e4ff7085d9
Nina <113557496+ninabraddock@users.noreply.github.com>
2023-10-24 11:00:58 -0400

    perhaps fixed a bug or two

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -1,10 +1,11 @@
-#Nina added a comment here
 from datetime import datetime
 
 # John bug space
 date_str = "20 22-03-17 10:45:30"
-date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+
+#switched data format 
+date_obj = datetime.strptime(date_str, '%y-%m-%d %H:%M:%S')
 # Format the date
-formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')
+formatted_date = date_obj.strftime('%y/%m/%d %H:%M:%S')
 
 print(formatted_date)
