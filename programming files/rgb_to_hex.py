0e4b36e3c8c4247e77b1632be82a24cf5fce3130
Nina <113557496+ninabraddock@users.noreply.github.com>
2023-10-24 10:46:28 -0400

    Added bugs :)

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,10 +1,10 @@
 #create function that est the color format 
-def rgb_to_hex(r, g, b):
+def rgb to_hex(r, g, b)
     r = max(0, min(255, r))
     g = max(0, min(255, g))
-    b = max(0, min(255, b))
-    return '{:02X}{:02X}{:02X}'.format(r, g, b)
+    b = max(0, min(255, b)
+    return '{:02X}{:02X}{:02X}'.format(r; g; b)
 
 
-test with hex_color = rgb_to_hex(255, 127, 0) # returns "FF7F00"
+test with hex_color == rgb_to_hex(255, 127, 0) # returns "FF7F00"
 
