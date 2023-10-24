28acc2b851db1594f0065e90cb2a9c8e026d289b
skyler heininger <122641443+SkylerHeininger@users.noreply.github.com>
2023-10-24 10:58:06 -0400

    bugs fixed in rgb_to_hex.py

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,10 +1,10 @@
 #create function that est the color format 
-def rgb to_hex(r, g, b)
+def rgb to_hex(r, g, b): # Missing colon
     r = max(0, min(255, r))
     g = max(0, min(255, g))
-    b = max(0, min(255, b)
-    return '{:02X}{:02X}{:02X}'.format(r; g; b)
+    b = max(0, min(255, b)) # Added missing paren
+    return '{:02X}{:02X}{:02X}'.format(r, g, b) # Removed semicolons
 
 
-test with hex_color == rgb_to_hex(255, 127, 0) # returns "FF7F00"
-
+test with hex_color = rgb_to_hex(255, 127, 0) # returns "FF7F00"
+# Fixed ==
