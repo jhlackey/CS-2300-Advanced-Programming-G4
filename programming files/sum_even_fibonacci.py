8e191950d9025faa1a92c70b9c0d1178f03495c8
jtomasi3 <123200835+jtomasi3@users.noreply.github.com>
2023-10-24 10:46:58 -0400

    Found bugs sum_even_fibonacci.py

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,9 +1,9 @@
 a, b = 0, 1
-sum_even = 
+sum_even =  //No value set
 while b < 4000000:
     if b % 2 == 0:
         sum_even += b
-    a, b = b, a+b
+    a, b = b, a+b //b should equal a + b && a doesn't change in value
 
 print(sum_even)
 
